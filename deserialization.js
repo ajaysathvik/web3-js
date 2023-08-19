70cf298baf5f60500e1fd20078a76b045627abbc
ajaysathvik <ajaysathvikk@gmail.com>
2023-08-19 18:56:30 +0530

    movie frontend and deserialisation

--- deserialization.js
+++ deserialization.js
@@ -2,14 +2,14 @@ const web3 = require("@solana/web3.js");
 
 // const Movie_Review_Program_Id = "CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDKgUduboN";
 
-function main() {
+async function main() {
   const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
   console.log("connection", connection);
-//   const accounts = await connection
-//     .getProgramAccounts(new web3.PublicKey(Movie_Review_Program_Id))
-//     .then((accounts) => {
-//       accounts.map(({ pubkey, account }) => {
-//         console.log(pubkey.toBase58(), account.data);
-//       })
-//     });
+  const accounts = await connection
+    .getProgramAccounts(new web3.PublicKey(Movie_Review_Program_Id))
+    .then((accounts) => {
+      accounts.map(({ pubkey, account }) => {
+        console.log(pubkey.toBase58(), account.data);
+      })
+    });
 }
